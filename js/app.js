dc553c9f7d9979d03594bea311cc556a16018896
Francesco Punta <puntafrancesco@gmail.com>
2023-02-10 16:54:26 +0100

    Modifiche function e style css, aggiunto header

--- js/app.js
+++ js/app.js
@@ -11,3 +11,37 @@ buttonEl.addEventListener(
        
 }
 )
+
+
+
+//funzione che mi genera una griglia
+function gridGenerate (grid){
+    grid.innerHTML = "";  // HTML svuotato per un altra partita
+    let tentativi = 0
+    
+    for(let i = 0; i < 100; i++){ // creo 100 div
+       const gridBox = document.createElement("div") // creo un div
+       gridBox.classList.add("box_easy") // accedo alla classlist di "div" aggiungo la classe "box"
+       gridBox.append ([i + 1])  // inserisco il numero progressivo nel box
+       
+       gridBox.addEventListener(
+           "click",
+           function (){
+               
+    for(let i = 0; i < arrayNumbersBomb.length; i++)
+    {
+        currentBombNUmber = arrayNumbersBomb[i]
+        if(this.innerHTML == currentBombNUmber){
+              this.classList.add("bomb")
+              // alert("hai perso")
+           } else {
+               this.classList.add("active")
+          }
+       }
+                      }
+       )
+
+       grid.append(gridBox) // inserisco il "div" nella griglia
+   }
+
+}
